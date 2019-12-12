7903942f6a71cf3bf58e534ccec01f54c3ebb06b
Costany93 <kandzaprince@gmail.com>
2019-12-12 11:20:45 +0000

    Je viens d'ajouter un tableau avec 2 thead et 4 tbody

--- second.php
+++ second.php
@@ -7,5 +7,15 @@
 		<p>Git hub</p>
 		<p>Un autre paragraphe ajouté seulement à second.php</p>
 		<h2>Un nouveau titre h2 a été ajouté</h2>
-	</body>
+			<thead>
+				<th>Nom</th>
+				<th>Prenom</th>
+			</thead>
+			<tbody>
+				<td>Je suis premier</td>
+				<td>Je suis le deuxieme</td>
+				<td>Je suis le troisieme</td>
+				<td>Je suis le quatrième</td>
+			</tbody>
+		</body>
 </html>
